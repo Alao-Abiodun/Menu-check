6b8a48991cb4e1861f4714cea04a485f392ca3b5
Alao-Abiodun <46402042+Alao-Abiodun@users.noreply.github.com>
2022-04-26 15:33:50 +0100

    chore/refractor the endpoint to get all menus

--- src/controllers/menus/menus.controller.ts
+++ src/controllers/menus/menus.controller.ts
@@ -4,9 +4,15 @@ import Menu from '../../models/menus/menus.model';
 
 const getMenus = async (req: Request, res: Response): Promise<any> => {
     try {
-        // find menus with typescript using aggregate method
-        const menus: IMenu[] = await Menu.aggregate([]);
-        return res.status(200).json({
+        // // find menus with typescript using aggregate method
+        // const menus: IMenu[] = await Menu.aggregate([]);
+        // return res.status(200).json({
+        //     success: true,
+        //     count: menus.length,
+        //     data: menus
+        // });
+        const menus: IMenu[] = await Menu.find();
+            return res.status(200).json({
             success: true,
             count: menus.length,
             data: menus
